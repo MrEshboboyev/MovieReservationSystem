ac250b8ddf5e44c7623e267dbd65b34a0aaed4a5
MrEshboboyev <mreshboboyev@gmail.com>
2024-09-14 11:39:59 +0500

    Authentication Services interface and implementation

---
+++ MovieReservationSystem.Application/Common/Models/LoginModel.cs
@@ -0,0 +1,8 @@
+﻿namespace MovieReservationSystem.Application.Common.Models
+{
+    public class LoginModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ MovieReservationSystem.Application/Services/Interfaces/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿using MovieReservationSystem.Application.Common.Models;
+using MovieReservationSystem.Domain.Entities;
+
+namespace MovieReservationSystem.Application.Services.Interfaces
+{
+    public interface IAuthService
+    {
+        Task<string> LoginAsync(LoginModel loginModel);
+        Task RegisterAsync(RegisterModel registerModel);
+        Task<string> GenerateJwtToken(ApplicationUser user, IEnumerable<string> roles);
+    }
+}
