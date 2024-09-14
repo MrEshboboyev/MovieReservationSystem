927cdf22bc5604872168813466789ea87db0ae77
MrEshboboyev <mreshboboyev@gmail.com>
2024-09-14 12:05:41 +0500

    identity, service lifetime, seeDatabase method();

--- MovieReservationSystem.Presentation/Program.cs
+++ MovieReservationSystem.Presentation/Program.cs
@@ -1,4 +1,7 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using MovieReservationSystem.Application.Common.Interfaces;
+using MovieReservationSystem.Domain.Entities;
 using MovieReservationSystem.Infrastructure.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -16,6 +19,14 @@ builder.Services.AddDbContext<AppDbContext>(option =>
     option.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+// adding identity
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
+
+// adding lifetime for services
+builder.Services.AddScoped<IDbInitializer, DbInitializer>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -30,5 +41,14 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
-
+SeedDatabase();
 app.Run();
+
+void SeedDatabase()
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var dbInitializer = scope.ServiceProvider.GetRequiredService<IDbInitializer>();
+        dbInitializer.Initialize();
+    }
+}
