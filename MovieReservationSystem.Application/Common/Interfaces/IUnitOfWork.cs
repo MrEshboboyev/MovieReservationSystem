993eea7168a6fcc40a01429fec71c8e1cb4d21e1
MrEshboboyev <mreshboboyev@gmail.com>
2024-09-14 20:06:38 +0500

    unit of work interface : Application : Create Common/Interfaces/IUnitOfWork.cs : all repo as fields and Save() method;

---
+++ MovieReservationSystem.Application/Common/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿namespace MovieReservationSystem.Application.Common.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        IActorRepository Actor { get; }
+        IMovieActorRepository MovieActor { get; }
+        IMovieRepository Movie { get; }
+        IScheduleRepository Schedule { get; }
+        ISeatRepository Seat { get; }
+        ITheaterRepository Theater { get; }
+        ITicketRepository Ticket { get; }
+
+        void Save();
+    }
+}
