22c49cfc29f84ccff1d750ff303fb270455d8cc7
MrEshboboyev <mreshboboyev@gmail.com>
2024-09-14 17:52:33 +0500

    service configuration for solution : Infrastructure : Create Configurations/ServiceConfig.cs : static , AddServices() extension method;

---
+++ MovieReservationSystem.Infrastructure/Configurations/ServiceConfig.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MovieReservationSystem.Application.Common.Interfaces;
+using MovieReservationSystem.Infrastructure.Data;
+
+namespace MovieReservationSystem.Infrastructure.Configurations
+{
+    public static class ServiceConfig
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            // adding lifetime
+            services.AddScoped<IDbInitializer, DbInitializer>();
+            return services;
+        }
+    }
+}
