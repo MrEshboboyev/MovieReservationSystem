57bcf25765a5ff9fcad2d7b1403f0c513a7472d5
MrEshboboyev <mreshboboyev@gmail.com>
2024-09-14 18:03:51 +0500

    db initializer method service : Infrastructure : Create Services/DbInitializerService.cs : static, SeedDatabase() extension method for IApplicationBuilder;

---
+++ MovieReservationSystem.Infrastructure/Services/DbInitializerService.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using MovieReservationSystem.Application.Common.Interfaces;
+
+namespace MovieReservationSystem.Infrastructure.Services
+{
+    public static class DbInitializerService
+    {
+        public static void SeedDatabase(this IApplicationBuilder app)
+        {
+            using (var scope = app.ApplicationServices.CreateScope())
+            {
+                var dbInitializer = scope.ServiceProvider.GetRequiredService<IDbInitializer>();
+                dbInitializer.Initialize();
+            }
+        }
+    }
+}
