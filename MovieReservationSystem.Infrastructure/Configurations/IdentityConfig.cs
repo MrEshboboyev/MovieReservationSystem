7c06bb7d39506a1ab8f1814a3f24cde6b88b3e96
MrEshboboyev <mreshboboyev@gmail.com>
2024-09-14 17:59:13 +0500

    Identity configuration for solution : Infrastructure : Create Configurations/IdentityConfig.cs : static , AddIdentityConfiguration() extension method;

---
+++ MovieReservationSystem.Infrastructure/Configurations/IdentityConfig.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using MovieReservationSystem.Domain.Entities;
+using MovieReservationSystem.Infrastructure.Data;
+
+namespace MovieReservationSystem.Infrastructure.Configurations
+{
+    public static class IdentityConfig
+    {
+        public static IServiceCollection AddIdentityConfiguration(this IServiceCollection services)
+        {
+            // adding identity
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddDefaultTokenProviders();
+
+            return services;
+        }
+    }
+}
