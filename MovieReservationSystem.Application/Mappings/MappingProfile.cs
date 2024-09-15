2990e227476c6f114d6840e4e9372e9a1ecf5939
MrEshboboyev <mreshboboyev@gmail.com>
2024-09-15 15:04:18 +0500

    mappig for seat reservation service : Application : Mappings/MappingProfile.cs : Ticket -> SeatReservationDTO, ReserveSeatDTO -> Ticket; Seat -> SeatDTO changed;

--- MovieReservationSystem.Application/Mappings/MappingProfile.cs
+++ MovieReservationSystem.Application/Mappings/MappingProfile.cs
@@ -6,14 +6,14 @@ namespace MovieReservationSystem.Application.Mappings
 {
     public class MappingProfile : Profile
     {
-        public MappingProfile() 
+        public MappingProfile()
         {
             // Movie -> MovieDTO
             CreateMap<Movie, MovieDTO>();
 
             // Movie -> MovieDetailsDTO (with actors and schedules)
             CreateMap<Movie, MovieDetailsDTO>()
-                .ForMember(dest => dest.Actors, 
+                .ForMember(dest => dest.Actors,
                 opt => opt.MapFrom(src => src.MovieActors.Select(ma => ma.Actor)))
                 .ForMember(dest => dest.Schedules, opt => opt.MapFrom(src => src.Schedules));
 
@@ -39,10 +39,23 @@ namespace MovieReservationSystem.Application.Mappings
                 .ForMember(dest => dest.TheaterName, opt => opt.MapFrom(src => src.Theater.Name));
 
             // Seat -> SeatDTO
-            CreateMap<Seat, SeatDTO>();
+            CreateMap<Seat, SeatDTO>()
+                .ForMember(dest => dest.IsAvailable, opt => opt.Ignore());
 
             // CreateScheduleDTO -> Schedule
             CreateMap<CreateScheduleDTO, Schedule>();
+
+            // Ticket -> SeatReservationDTO
+            CreateMap<Ticket, SeatReservationDTO>()
+                .ForMember(dest => dest.MovieTitle, opt => opt.MapFrom(src => src.Schedule.Movie.Title))
+                .ForMember(dest => dest.TheaterName, opt => opt.MapFrom(src => src.Schedule.Theater.Name))
+                .ForMember(dest => dest.SeatNumber, opt => opt.MapFrom(src => src.Seat.SeatNumber))
+                .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.User.UserName))
+                .ForMember(dest => dest.ShowTime, opt => opt.MapFrom(src => src.Schedule.ShowTime));
+
+            // ReserveSeatDTO -> Ticket
+            CreateMap<ReserveSeatDTO, Ticket>()
+                .ForMember(dest => dest.PurchaseDate, opt => opt.MapFrom(src => DateTime.UtcNow));
         }
     }
 }
