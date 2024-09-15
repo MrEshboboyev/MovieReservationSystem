69f2e5b01c96f01b243a2edd3ed035dd3204a6f1
MrEshboboyev <mreshboboyev@gmail.com>
2024-09-15 22:53:25 +0500

    Payment entity created

---
+++ MovieReservationSystem.Domain/Entities/Payment.cs
@@ -0,0 +1,18 @@
+﻿namespace MovieReservationSystem.Domain.Entities
+{
+    public class Payment
+    {
+        public Guid PaymentId { get; set; }
+        public string StripePaymentId { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime PaymentDate { get; set; }
+        public string Status { get; set; }
+
+
+        // relations (user and ticket)
+        public Guid TicketId { get; set; }
+        public Ticket Ticket { get; set; }
+        public string UserId { get; set; }
+        public ApplicationUser User { get; set; }
+    }
+}
