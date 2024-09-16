70ee91ac710b7ad2b65d6fb25ccb2329dee024b0
MrEshboboyev <mreshboboyev@gmail.com>
2024-09-16 10:22:36 +0500

    Payment Repo implemented

---
+++ MovieReservationSystem.Infrastructure/Repositories/PaymentRepository.cs
@@ -0,0 +1,16 @@
+﻿using MovieReservationSystem.Application.Common.Interfaces;
+using MovieReservationSystem.Domain.Entities;
+using MovieReservationSystem.Infrastructure.Data;
+
+namespace MovieReservationSystem.Infrastructure.Repositories
+{
+    public class PaymentRepository(AppDbContext db) : Repository<Payment>(db), IPaymentRepository
+    {
+        private readonly AppDbContext _db = db;
+
+        public void Update(Payment payment)
+        {
+            _db.Payments.Update(payment);
+        }
+    }
+}
