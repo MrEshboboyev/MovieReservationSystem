afa7705c8d6ac63133427fe93571641ca7ecccf8
MrEshboboyev <mreshboboyev@gmail.com>
2024-09-16 10:20:16 +0500

    Payment Repo interface

---
+++ MovieReservationSystem.Application/Common/Interfaces/IPaymentRepository.cs
@@ -0,0 +1,9 @@
+﻿using MovieReservationSystem.Domain.Entities;
+
+namespace MovieReservationSystem.Application.Common.Interfaces
+{
+    public interface IPaymentRepository : IRepository<Payment>
+    {
+        void Update(Payment payment);
+    }
+}
