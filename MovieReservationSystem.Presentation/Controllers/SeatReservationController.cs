637a6fef19bb0e7c8f5e5df4b87d2e61be1b42f9
MrEshboboyev <mreshboboyev@gmail.com>
2024-09-18 10:43:31 +0500

    create seat get endpoints;

---
+++ MovieReservationSystem.Presentation/Controllers/SeatReservationController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieReservationSystem.Application.Services.Interfaces;
+using System.Security.Claims;
+
+namespace MovieReservationSystem.Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SeatReservationController(ISeatReservationService seatReservationService) : ControllerBase
+    {
+        private readonly ISeatReservationService _seatReservationService = seatReservationService;
+
+        #region Private Methods
+        private string GetUserId() => User.FindFirstValue(ClaimTypes.NameIdentifier) 
+            ?? throw new Exception("Login required!");
+        #endregion
+
+        [HttpGet("get-available-seats")]
+        public async Task<IActionResult> GetAvailableSeats(Guid scheduleId)
+        {
+            try
+            {
+                return Ok(await _seatReservationService.GetAvailableSeatsAsync(scheduleId));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("get-user-reservations")]
+        public async Task<IActionResult> GetUserReservations()
+        {
+            try
+            {
+                return Ok(await _seatReservationService.GetUserReservationsAsync(GetUserId()));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("get-schedule-reservations")]
+        public async Task<IActionResult> GetScheduleReservations(Guid scheduleId)
+        {
+            try
+            {
+                return Ok(await _seatReservationService.GetReservationsForScheduleAsync(scheduleId));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("check-available-seat")]
+        public async Task<IActionResult> GetSeatAvailability(Guid seatId, Guid scheduleId)
+        {
+            try
+            {
+                return Ok(await _seatReservationService.IsSeatAvailableAsync(scheduleId, seatId));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
